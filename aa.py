855a312af08fba2694ff243e28bcf7650a5d2be7
sandeepR27 <sandeeprrk27@gmail.com>
2023-03-02 16:22:44 +0530

    Update

---
+++ aa.py
@@ -0,0 +1,5 @@
+s = 'suriya'
+
+ASCII_values = [ord(character) for character in s]
+
+print(ASCII_values)
---
+++ demo.py
@@ -0,0 +1,4 @@
+alist=[22,1,99,11,2]
+for index_x in range(1,len(alist)):
+    avalue=alist[index_x]
+    print(avalue)
